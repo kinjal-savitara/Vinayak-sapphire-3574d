9e7200adad8cda5e4820ba74cfd7af4a78919c26
kinjal-savitara <kinjal@savitarainfotel.com>
2025-10-16 17:35:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,64 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import  { useState, useEffect }  from "react";
 
 export default function Home() {
+  const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
+  const [submitted, setSubmitted] = useState(false);
+  const [iframeUrl, setIframeUrl] = useState("");
+
+   
+
+
+  useEffect(() => {
+    document.documentElement.style.height = "100%";
+    document.body.style.height = "100%";
+    document.body.style.margin = "0";
+
+    // Get query param "tour"
+    const params = new URLSearchParams(window.location.search);
+    const tour = params.get("tour");
+
+    // Map query param to iframe links
+    let url = "https://biganto.com/tour/46783/?iframe=1"; // default
+    if (tour === "interior") {
+      url = "https://biganto.com/tour/46783/?iframe=1";
+    } else  {
+      url = "https://biganto.com/tour/46783/?iframe=1";
+    }
+
+    setIframeUrl(url);
+  }, []);
+
+  
+  
+    const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+
+ 
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+     <div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: 9999 }}>
+    
+    {iframeUrl != ''?
+      <iframe 
+        src={iframeUrl}
+        frameBorder="0"
+        scrolling="no"
+        width="100%"
+        height="100%"
+        allow="vr;xr-spatial-tracking;gyroscope;accelerometer;magnetometer;"
+        title="Vinayak sapphire"
+         allowfullscreen="allowfullscreen"
+        style={{ display: "block", border: "none" }}
+      ></iframe>:null}
+ 
+           
     </div>
+
+
+   
   )
 }
